deeb295e1682ec4ffac4da4a4ef8f1bcef3fe181
ch00me <anijam12@naver.com>
2023-05-22 20:19:45 +0900

    [fix]

--- drowsy_detection_realtime_flask.py
+++ drowsy_detection_realtime_flask.py
@@ -140,4 +140,4 @@ def drowsy_detection_finish():
 
 # Flask 앱 실행
 if __name__ == '__main__' :
-    app.run(host = '127.0.0.1', port = 5000, debug = True)
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
